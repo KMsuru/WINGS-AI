97e28aeb3b27059e6a2d7c288366e0ff5531158b
Shruti Kolmadrekar <kmsuru46@gmail.com>
2026-09-03 20:49:22 +0530

    Completed authentication and JWT

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
 from app.routes import main
+from app.extensions import db, bcrypt, jwt
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["JWT_SECRET_KEY"] = "wings-ai-secret-key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///wings_ai.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    
+    from app.models import User
+
+    with app.app_context():
+        db.create_all()
 
     app.register_blueprint(main)
 
--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,10 +1,77 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from flask_jwt_extended import create_access_token, jwt_required
+from app.models import User
+from app.extensions import db
 
 main = Blueprint("main", __name__)
+
 
 @main.route("/api/health", methods=["GET"])
 def health_check():
     return {
         "status": "success",
         "message": "WINGS AI backend is running"
+    }
+
+
+@main.route("/api/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    if not data or not data.get("name"
+                                ) or not data.get("email") or not data.get("password"):
+        return {
+            "message": "Name, email and password are required"
+        }, 400
+    existing_user = User.query.filter_by(email=data["email"]).first()
+
+    if existing_user:
+        return {
+            "message": "Email already registered"
+        }, 400
+        
+    user = User(
+        name=data["name"],
+        email=data["email"]
+    )
+    
+    user.set_password(data["password"])
+    
+    db.session.add(user)
+    db.session.commit()
+    return {
+        "message": "User registered successfully"
+    }, 201
+
+@main.route("/api/login", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    if not data or not data.get("email") or not data.get("password"):
+        return {
+            "message": "Email and password are required"
+        }, 400
+    user = User.query.filter_by(email=data["email"]).first()
+    
+    if not user:
+        return {
+            "message": "Invalid email or password"
+        }, 401
+        
+    access_token = create_access_token(identity=str(user.id))
+        
+    if not user.check_password(data["password"]):
+        return {
+            "message": "Invalid email or password"
+        }, 401
+    
+    return {
+        "message": "Login successful",
+        "access_token": access_token
+    }
+    
+@main.route("/api/profile", methods=["GET"])
+@jwt_required()
+def profile():
+    return {
+        "message": "Profile API working"
     }
